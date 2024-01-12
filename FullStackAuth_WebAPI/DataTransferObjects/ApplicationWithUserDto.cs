ae14ffbac385f8d4099f80491390f875ee130be7
mukhsia <andre.n.mukhsia@gmail.com>
2024-01-11 17:32:41 -0800

    Created ApplicationWithUserDto Data Transfer Object.

---
+++ FullStackAuth_WebAPI/DataTransferObjects/ApplicationWithUserDto.cs
@@ -0,0 +1,15 @@
+﻿namespace FullStackAuth_WebAPI.DataTransferObjects
+{
+    public class ApplicationWithUserDto
+    {
+
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Archived { get; set; }
+        public string Status { get; set; }
+        public string Company { get; set; }
+        public UserForDisplayDto Owner { get; set; }
+
+    }
+}
